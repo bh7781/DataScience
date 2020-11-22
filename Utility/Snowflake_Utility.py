c44a8e70ef0a0d447d6dec294897d67879692411
Chinmay Shelke <Chinmay.Shelke@eclerx.com>
2020-11-22 13:04:15 +0530

    Snowflake_Utility : Added new method to capitalize name field

--- Utility/Snowflake_Utility.py
+++ Utility/Snowflake_Utility.py
@@ -61,3 +61,18 @@ def is_valid_password1(pw_string):
     else:
         print("Password invalid !!")
         return bool(0)
+
+def capitalize_name(input_name):
+    """
+    Method to capitalize first character of name.
+    If '.' is present in string then first character of every list 
+    element after splitting input string by '.'  is capitalized.
+    This method should be called iff is_valid_name() returns True.
+    Input : name (allowed)
+    Output : Capitalized name
+    """
+    splitted_name = input_name.split('.')
+    word_list = []
+    for word in splitted_name:
+        word_list.append(word[0].upper() + word[1:])
+    return ('.'.join(word_list))
